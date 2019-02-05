fcfc34b8863669a2fd053aab50c9512ce7f67728
Yann Raffin <yannraffin@Yann-Raffin-Gekko.local>
2019-02-05 15:32:12 +0100

    Custom admin : add Document classes + IP adress for the file upload.

--- home/admin.py
+++ home/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from home.models import Document
 
-# Register your models here.
+
+@admin.register(Document)
+class DocumentAdmin(admin.ModelAdmin):
+    list_display = ('upload', 'upload_user', 'ip_address', 'uploaded_at', 'comment')
